f2ecdb3b47307a97e1df7ae19dcda2c8bb5db4e9
Venky <venkatramnm@gmail.com>
2012-08-02 18:08:41 +0530

    Endian Support for reading XML Files

--- src/main/java/com/venky/xml/XMLDocument.java
+++ src/main/java/com/venky/xml/XMLDocument.java
@@ -2,7 +2,6 @@ package com.venky.xml;
 
 import java.io.File;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Reader;
@@ -68,8 +67,7 @@ public class XMLDocument {
     }
 
     public static XMLDocument getDocumentFor(InputStream is) {
-        Reader r = new InputStreamReader(is);
-        return getDocumentFor(r);
+       return new XMLDocument(XMLSerializationHelper.getDocument(is));
     }
     
     public static XMLDocument getDocumentFor(String s){
--- src/main/java/com/venky/xml/XMLSerializationHelper.java
+++ src/main/java/com/venky/xml/XMLSerializationHelper.java
@@ -5,8 +5,10 @@ package com.venky.xml;
 //import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.Writer;
@@ -20,6 +22,9 @@ import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSOutput;
 import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.InputSource;
+
+import com.venky.core.io.ByteArrayInputStream;
+import com.venky.core.string.StringUtil;
 
 public abstract class XMLSerializationHelper {
 	private static DOMImplementationRegistry registry = null ; 
@@ -60,11 +65,7 @@ public abstract class XMLSerializationHelper {
 	public final static int MAXBOMBYTES = 4;// no bom sequence is longer than 4
 											// byte
 
-	public static int getBOMType(byte[] _bomBytes) {
-		return getBOMType(_bomBytes, _bomBytes.length);
-	}
-
-	public static int getBOMType(byte[] _bomBytes, int _length) {
+	private static int getBOMType(byte[] _bomBytes, int _length) {
 		for (int i = 0; i < BOMBYTES.length; i++) {
 			for (int j = 0; j < _length && j < BOMBYTES[i].length; j++) {
 				if (_bomBytes[j] != BOMBYTES[i][j])
@@ -77,51 +78,65 @@ public abstract class XMLSerializationHelper {
 		return NONE;
 	}
 
-	public static int getBOMType(File _f) throws IOException {
-		FileInputStream fIn = new FileInputStream(_f);
-		byte[] buff = new byte[MAXBOMBYTES];
-		int read = fIn.read(buff);
-		int BOMType = getBOMType(buff, read);
-		fIn.close();
-		return BOMType;
-	}
-
-	public static int getSkipBytes(int BOMType) {
+	private static int getSkipBytes(int BOMType) {
 		if (BOMType < 0 || BOMType >= BOMBYTES.length)
 			return 0;
 		return BOMBYTES[BOMType].length;
 	}
-
-	/**
-	 * Just reads necessary bytes from the stream
-	 * 
-	 * @param _fIn
-	 */
-	public static Reader getReader(File _f) throws IOException {
-		int BOMType = getBOMType(_f);
-		int skipBytes = getSkipBytes(BOMType);
-		FileInputStream fIn = new FileInputStream(_f);
-		fIn.skip(skipBytes);
-		String e = "UTF-8";
-		if (BOMType == UTF16BE || BOMType == UTF16LE) {
-			e = "UTF-16";
-		} else if (BOMType == UTF32BE || BOMType == UTF32BE) {
-			e = "UTF-32";
+	public static Reader getReader(InputStream is){
+		InputStream in = is; 
+		if (!is.markSupported()){
+			in = new ByteArrayInputStream(StringUtil.readBytes(is));
 		}
-		Reader reader = new InputStreamReader(fIn, e);
-		return reader;
-	}
 
-	public static Document getDocument(File _f) {
+		in.mark(MAXBOMBYTES + 1);
+		byte[] buff = new byte[MAXBOMBYTES];
+		int read;
 		try {
-			DOMParser parser = new DOMParser();
-			parser.parse(new InputSource(getReader(_f)));
-			return parser.getDocument();
-		} catch (Exception e) {
+			read = in.read(buff);
+			int BOMType = getBOMType(buff, read);
+			in.reset();
+			in.skip(getSkipBytes(BOMType));
+			
+			
+			String charSetName = "UTF-8";
+			switch (BOMType){
+				case UTF16BE:
+					charSetName ="UTF-16BE";
+					break;
+				case UTF16LE:
+					charSetName ="UTF-16LE";
+					break;
+				case UTF32BE:
+					charSetName ="UTF-32BE";
+					break;
+				case UTF32LE:
+					charSetName ="UTF-32LE";
+					break;
+			}
+			return new InputStreamReader(in, charSetName);
+		} catch (IOException e1) {
+			throw new RuntimeException(e1);
+		}
+	}
+	
+
+	private static Reader getReader(File _f)  {
+		try {
+			return getReader(new FileInputStream(_f));
+		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
+	public static Document getDocument(File _f) {
+		return getDocument(getReader(_f));
+	}
+	
+	public static Document getDocument(InputStream is) {
+		return getDocument(getReader(is));
+	}
+	
 	public static Document getDocument(Reader reader) {
 		try {
 			DOMParser parser = new DOMParser();
